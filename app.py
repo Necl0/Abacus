e0ca8f58b4fcf50d2ed5eaaf5641c5226df113c2
Josiah <63004866+Necl0@users.noreply.github.com>
2022-11-02 16:23:24 -0400

    Feat: Added backend functionality

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import pickle
 
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 def predict_digit(img):
@@ -17,14 +18,14 @@ def predict_digit(img):
     prediction = model.predict(img)
     return prediction
 
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile = request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
 
-@app.route('/upload', methods=['POST'])
-def result():
-    if request.method == 'POST':
-        img = request.files['img']
-        prediction = predict_digit(img)
-        return render_template('index.html', prediction=prediction)
-
+    prediction = predict_digit(imagefile)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
