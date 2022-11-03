a39577c6684101d8fb6cac53554fd9788be0bb90
Josiah <63004866+Necl0@users.noreply.github.com>
2022-11-02 23:34:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from flask import Flask, render_template, request
 import pickle
 from PIL import Image
 import numpy as np
+from werkzeug.datastructures import FileStorage
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
+    """Basic route"""
     return render_template('index.html')
 
 
-def predict_digit(img):
+def predict_digit(img) -> list[int]:
+    """prediction function"""
     # load model
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -19,25 +22,24 @@ def predict_digit(img):
     prediction = model.predict(img)
     return prediction
 
+
 @app.route('/', methods=['POST'])
-def predict():
-    imagefile = request.files['imagefile']
+def predict() -> str:
+    """Main backend driving function"""
+    imagefile: FileStorage = request.files['imagefile']
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)
-    # load imagefile content from image_path
-    img = Image.open(image_path)
+    img = Image.open(image_path).convert('L')
 
     # preprocess image
     img = img.resize((28, 28))
-    img = img.convert('L')
     img = np.array(img)
     img = img.reshape(1, 784)
 
+    prediction = predict_digit(img)[0]
 
-    prediction = predict_digit(img)
+    return render_template('index.html', prediction=prediction)
 
-
-    return render_template('index.html', prediction=prediction[0])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
