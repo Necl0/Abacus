c38302e7380840cff14c08bee121403821d8b486
Josiah <63004866+Necl0@users.noreply.github.com>
2022-11-03 15:57:24 -0400

    Update pkl_compress.py

--- pkl_compress.py
+++ pkl_compress.py
@@ -2,19 +2,8 @@ import bz2
 import pickle
 import _pickle as cPickle
 
-#compress pickle file
-def compress_pickle(title, data):
-    with bz2.BZ2File(title + '.pbz2', 'w') as f:
-        cPickle.dump(data, f)
-
 # Load any compressed pickle file
 def decompress_pickle(file):
     data = bz2.BZ2File(file, 'rb')
     data = cPickle.load(data)
     return data
-
-#load model.pkl and compress
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-compress_pickle('model', model)
